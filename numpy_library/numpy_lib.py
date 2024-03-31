957bf8d28c1901ea4dc7e8486177998baca69888
Parviz <perviz.pirizade@gmail.com>
2024-03-31 13:08:25 +0200

    test commit

---
+++ numpy_library/numpy_lib.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.array([1,2,3,4])
+
+print(type(a))
+
+
+#a.size
+
+#a.ndim  rank of the array
+
+#a.shape
+
+
+a[0] = 12;
+c = a[0:4];
+print(c)
+
+
+
+#vector addition
+
+u= np.array([[0],[1]])
+v = np.array([[2],[1]])
+
+
+print(np.dot(u.T,v))
+
+print(u-v)
+
+print(u+v) #element wise addidition
+
+
+#mapping
+
+
+x =  np.array([0,np.pi/2,np.pi])  
+
+y=np.sin(x)   #universal function
+
+#np.linspace
+
+
+x = np.linspace(0,1 ,10)
+
+y =np.sin(x)
+
+
+plt.plot(x,y)
