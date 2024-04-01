d790ceb18addf27595ea94fdd5fa3f889fb3cc3d
Parviz <perviz.pirizade@gmail.com>
2024-04-01 23:40:24 +0200

    post  and get requests added , previous requests file was deleted

--- rest_api/requests_module.py
+++ rest_api/requests_module.py
@@ -8,6 +8,34 @@ print(r.headers['date'])
 print(r.headers['content-type'])
 print(r.text)'''
 
-base_url = 'httbin.org'
+base_url = 'http://httbin.org/get'
+
+payload ={"name":"Parviz","ID":"123"}
+
+r =  requests.get(base_url,params=payload)
+
+'''print(r.url)
+print(r.request.body)
+print(r.text)
+print(r.status_code)
+print(r.headers['Content-type'])
+print(r.json())
+'''
+
+payload ={"name":"Parviz","ID":"123"}
+
+url_post = 'http://httpbin.org/post'
+r_post = requests.post(url_post,data=payload)
+
+#print(r_post.url)
+
+
+
+#print(r_post.request.body)
+print(r_post.request.body)
+#why only post has a body
+
+
+
 
 
