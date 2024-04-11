efbd7f899deff23ceef478ceb145ee04b7a5fd13
Parviz <perviz.pirizade@gmail.com>
2024-04-11 21:14:37 +0200

    basic  list comprehension operations demonstrated

---
+++ list_comprehensions/list_comprehensions.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import math
+
+
+l1 = [1,12,3,4,5]
+
+l2 = [item**2 for item in l1]
+
+print(l2)
+
+l3 = [int(math.sqrt(item))  for item in l2]
+
+print(l3)
+
+l4 =  [item for item in l3 if item>10]
