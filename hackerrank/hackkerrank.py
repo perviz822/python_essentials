bb4759508e77c07350fcefbbf91a42d7572ed20a
Parviz <perviz822@gmail.com>
2024-04-13 00:24:01 +0200

    new commit

--- hackerrank/hackkerrank.py
+++ hackerrank/hackkerrank.py
@@ -163,7 +163,7 @@ def print_door_mat ():
             print('-'* int((args[1]-7)//2) , end ='')
         print(('-')*(int((args[1]-t*3)//2)) , end ='')
 
-        print('\n')  
+        print()  
 
         t=t+2 if i !=int((args[0]-1)//2) and i<int((args[0]-1)//2) else t-2
     
