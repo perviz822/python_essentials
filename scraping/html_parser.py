eccdf7615503ff6a1013a59c92d80472e563f931
Parviz <perviz822@gmail.com>
2024-04-22 20:51:17 +0200

    greedy algorithm for knapsack problem added and,  html parser problem1 solved

---
+++ scraping/html_parser.py
@@ -0,0 +1,28 @@
+from html.parser import HTMLParser
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+# create a subclass and override the handler methods
+class Parser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print ("Start :", tag)
+        self.print_attr(attrs)
+    def handle_endtag(self, tag):
+        print ("End   :", tag)
+    def handle_startendtag(self, tag, attrs):
+        print ("Empty :", tag)
+        self.print_attr(attrs)
+        
+        
+    def print_attr(self,attrs):
+      for attr, value  in attrs:
+          print("->{}>{}".format(attr,value))
+
+# Creating an instance of our class.
+parser = Parser()
+# Poviding the input.
+parser.feed('<html><title class = "t1">Satyam Blog</title><body><p>'
+            'I love Tea.</p><'
+            '/body><!--Just started--></html>')
